4a7ded0895a68d7be374dc7abc93c9e7f70f1c0e
richhost <hexcy@pm.me>
2024-12-06 18:11:53 +0800

    fix mobile nav

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -13,9 +13,11 @@ export function MobileNav() {
 
       <Drawer.Portal>
         <Drawer.Overlay className="bg-black/40 fixed inset-0 z-30" />
-        <Drawer.Content className="fixed max-h-[60%] inset-x-0 bottom-0 z-30 overflow-auto bg-white p-5 rounded-t-xl">
+        <Drawer.Content className="fixed flex flex-col max-h-[60svh] inset-x-0 bottom-0 py-6 z-30 bg-white rounded-t-xl">
           <Drawer.Title />
-          <Nav />
+          <div className="overflow-auto px-6">
+            <Nav />
+          </div>
         </Drawer.Content>
       </Drawer.Portal>
     </Drawer.Root>
