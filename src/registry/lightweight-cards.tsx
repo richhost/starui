9e0258526dc7dd1d6f2ecaa515087f4fd9c22f27
richhost <hexcy@pm.me>
2025-01-12 16:02:33 +0800

    update: Light Weight Cards

--- src/registry/lightweight-cards.tsx
+++ src/registry/lightweight-cards.tsx
@@ -45,8 +45,9 @@ function Card({
       )}
     >
       <div
+        tabIndex={0}
         className={cn(
-          "group-hover:-translate-y-12 transition duration-500 ease-in-out rounded-md border-2 border-black/5 -skew-y-6 backdrop-blur-sm relative after:absolute after:inset-0 after:-z-10 after:rounded-[inherit] after:bg-linear-to-r after:from-white/50 after:to-transparent after:pointer-events-none",
+          "focus:-translate-y-12 group-hover:-translate-y-12 transition duration-500 ease-in-out rounded-md border-2 border-black/5 -skew-y-6 backdrop-blur-sm relative after:absolute after:inset-0 after:-z-10 after:rounded-[inherit] after:bg-linear-to-r after:from-white/50 after:to-transparent after:pointer-events-none",
           innerClassName
         )}
       >
