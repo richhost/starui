35dd329e3712e196e878ad87f898fa5ead89690a
richhost <hexcy@pm.me>
2025-02-06 15:40:35 +0800

    feat: some docs

--- src/example/flip-text-demo.tsx
+++ src/example/flip-text-demo.tsx
@@ -1,5 +1,10 @@
 import FlipText from "@/registry/flip-text";
 
 export default function FlipTextDemo() {
-	return <FlipText words={["UI UX", "Product", "Motion"]} />;
+  return (
+    <div className="flex items-center text-4xl">
+      <FlipText words={["UI UX", "Product", "Motion"]} delay={2000} />
+      <span>{"\u00A0"}Design</span>
+    </div>
+  );
 }
--- src/registry/flip-text.tsx
+++ src/registry/flip-text.tsx
@@ -3,71 +3,76 @@
 import { useMemo, useState, useEffect } from "react";
 import { AnimatePresence, motion } from "motion/react";
 import useMeasure from "react-use-measure";
+import { cn } from "@/lib/utils";
 
 interface FlipTextProps {
-	words: string[];
-	duration?: number;
+  words: string[];
+  delay?: number;
+  className?: string;
 }
 
-export default function FlipText({ words, duration = 3000 }: FlipTextProps) {
-	const [index, setIndex] = useState(0);
+export default function FlipText({
+  words,
+  delay = 3000,
+  className,
+}: FlipTextProps) {
+  const [index, setIndex] = useState(0);
 
-	const max = useMemo(
-		() => Math.max(...words.map((word) => word.length)),
-		[words],
-	);
+  const max = useMemo(
+    () => Math.max(...words.map((word) => word.length)),
+    [words]
+  );
 
-	const letters = useMemo(() => words[index].split(""), [index]);
+  const letters = useMemo(() => words[index].split(""), [index]);
 
-	const [ref, { width }] = useMeasure();
+  const [ref, { width }] = useMeasure();
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setIndex((index) => (index + 1) % words.length);
-		}, duration);
-		return () => clearInterval(interval);
-	}, [duration]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((index) => (index + 1) % words.length);
+    }, delay);
 
-	return (
-		<div className="flex flex-col">
-			<h3 className="text-2xl lg:text-7xl font-semibold flex">
-				<motion.span
-					aria-hidden={true}
-					className="inline-block overflow-hidden relative text-nowrap"
-					animate={{ width: width ? width : undefined }}
-					transition={{ duration: 0.3 }}
-				>
-					<span
-						ref={ref}
-						className="inline-block opacity-0 pointer-events-none absolute"
-					>
-						{words[index]}
-					</span>
+    return () => clearInterval(interval);
+  }, []);
 
-					{Array.from({ length: max }).map((_, i) => {
-						return (
-							<AnimatePresence key={i} mode="wait" initial={false}>
-								<motion.span
-									key={`${words[index]}-${i}`}
-									initial={{ rotateY: "-0.25turn" }}
-									animate={{ rotateY: "0turn" }}
-									exit={{ rotateY: "0.25turn" }}
-									className="inline-block"
-									transition={{
-										type: "tween",
-										delay: 0.1 + i * 0.01,
-										duration: 0.1,
-									}}
-								>
-									{letters[i] === " " ? "\u00A0" : letters[i]}
-								</motion.span>
-							</AnimatePresence>
-						);
-					})}
-				</motion.span>
+  return (
+    <motion.span
+      aria-hidden={true}
+      className={cn(
+        "inline-block overflow-hidden relative text-nowrap",
+        className
+      )}
+      animate={{ width: width ? width : undefined }}
+      transition={{ duration: 0.3 }}
+    >
+      <span
+        ref={ref}
+        className="inline-block opacity-0 pointer-events-none absolute"
+      >
+        {words[index]}
+      </span>
 
-				<span>{"\u00A0"}Design</span>
-			</h3>
-		</div>
-	);
+      {Array.from({ length: max }).map((_, i) => {
+        return (
+          <AnimatePresence key={i} mode="wait" initial={false}>
+            <motion.span
+              key={`${words[index]}-${i}`}
+              initial={{ rotateY: "-0.25turn" }}
+              animate={{ rotateY: "0turn" }}
+              exit={{ rotateY: "0.25turn" }}
+              className="inline-block"
+              transition={{
+                type: "tween",
+                delay: 0.1 + i * 0.01,
+                duration: 0.1,
+              }}
+              aria-hidden
+            >
+              {letters[i] === " " ? "\u00A0" : letters[i]}
+            </motion.span>
+          </AnimatePresence>
+        );
+      })}
+    </motion.span>
+  );
 }
